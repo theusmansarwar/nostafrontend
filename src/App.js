e4b7280cb32eec50db75f5c0e8342eaa023fa52e
Muhammad Usman Sarwar <105904144+theusmansarwar@users.noreply.github.com>
2025-04-16 13:29:37 +0500

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { motion } from "framer-motion";
+function App() {
+  return (
+    <div className="App">
+    <div className="coming-soon-container">
+      <div className="coming-soon-box">
+        <h1 className="title">NostaPay</h1>
+        <p className="subtitle">Website Coming Soon</p>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
